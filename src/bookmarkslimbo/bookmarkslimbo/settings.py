80d533fe99197ef6c4d77f5771814ca5c932adad
kamil-s-solecki <kamsolecki@gmail.com>
2019-03-15 14:27:21 +0100

    Allow only authenticated users to access endpoints

--- src/bookmarkslimbo/bookmarkslimbo/settings.py
+++ src/bookmarkslimbo/bookmarkslimbo/settings.py
@@ -125,7 +125,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
 }
