c9b0cb9076c4701ae9838377248a9ba1b54f0949
kamil-s-solecki <kamsolecki@gmail.com>
2019-03-18 18:00:35 +0100

    Validate link field

--- src/bookmarkslimbo/bookmarks/models.py
+++ src/bookmarkslimbo/bookmarks/models.py
@@ -6,7 +6,7 @@ class Bookmark(models.Model):
     expiration = models.DateTimeField()
     title = models.CharField(max_length=128)
     description = models.CharField(max_length=255, blank=True, null=True)
-    link = models.CharField(max_length=255)
+    link = models.URLField()
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
