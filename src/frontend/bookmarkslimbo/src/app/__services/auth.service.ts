0c584b99fd4d51bc50d1f2ef8cbe63e879faf0e0
kamil-s-solecki <kamsolecki@gmail.com>
2019-03-29 18:46:48 +0100

    Store username in localstorage

--- src/frontend/bookmarkslimbo/src/app/__services/auth.service.ts
+++ src/frontend/bookmarkslimbo/src/app/__services/auth.service.ts
@@ -9,19 +9,33 @@ export class AuthService {
   private user: User | null = null;
 
   isAuthenticated() {
-    return !!this.user;
+    return null !== this.getUser();
   }
 
   login(username: string, password: string): Observable<any> {
     if (password === 'password') {
-      this.user = { username };
+      this.setUser({ username });
       return of(null);
     } else {
       return new Observable(subscriber => subscriber.error());
     }
   }
 
+  public getUser() {
+    if (!this.user) {
+      const userSlug = localStorage.getItem('user');
+      this.user = userSlug ? JSON.parse(userSlug) : null;
+    }
+    return this.user;
+  }
+
   logout() {
     this.user = null;
+    localStorage.removeItem('user');
+  }
+
+  private setUser(user: User) {
+    this.user = user;
+    localStorage.setItem('user', JSON.stringify(user));
   }
 }
