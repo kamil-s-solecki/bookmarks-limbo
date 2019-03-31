35d3a093a214d4ee2933f49d888d5ff8f11cca9c
kamil-s-solecki <kamsolecki@gmail.com>
2019-03-31 19:14:47 +0200

    Implement expiration count pipe

--- src/frontend/bookmarkslimbo/src/app/__pipes/expiration-count.pipe.spec.ts
+++ src/frontend/bookmarkslimbo/src/app/__pipes/expiration-count.pipe.spec.ts
@@ -1,8 +1,62 @@
 import { ExpirationCountPipe } from './expiration-count.pipe';
+import * as moment from 'moment';
 
 describe('ExpirationCountPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ExpirationCountPipe();
-    expect(pipe).toBeTruthy();
+  const pipe = new ExpirationCountPipe();
+
+  it('counts years', () => {
+    const later = moment('2003-01-01');
+    const now = moment('2000-01-01'); 
+
+    expect(pipe.expirationCount(now, later)).toBe('3 years');
+  });
+
+  it('counts months', () => {
+    const later = moment('2000-04-01');
+    const now = moment('2000-01-01'); 
+
+    expect(pipe.expirationCount(now, later)).toBe('3 months');
+  });
+
+  it('counts years and months', () => {
+    const later = moment('2003-04-01');
+    const now = moment('2000-01-01'); 
+
+    expect(pipe.expirationCount(now, later)).toBe('3 years, 3 months');
+  });
+
+  it('counts months and days', () => {
+    const later = moment('2000-04-10');
+    const now = moment('2000-01-01'); 
+
+    expect(pipe.expirationCount(now, later)).toBe('3 months, 9 days');
+  });
+
+  it('counts days between months', () => {
+    const later = moment('2000-04-02');
+    const now = moment('2000-03-30'); 
+
+    expect(pipe.expirationCount(now, later)).toBe('3 days');
+  });
+
+  it('counts hours', () => {
+    const later = moment('2000-01-01T03:00:00');
+    const now = moment('2000-01-01T00:00:00'); 
+
+    expect(pipe.expirationCount(now, later)).toBe('3 hours');
+  });
+
+  it('counts minutes', () => {
+    const later = moment('2000-01-01T00:03:00');
+    const now = moment('2000-01-01T00:00:00'); 
+
+    expect(pipe.expirationCount(now, later)).toBe('3 minutes');
+  });
+
+  it('hurry up!', () => {
+    const later = moment('2000-01-01T00:00:05');
+    const now = moment('2000-01-01T00:00:00'); 
+
+    expect(pipe.expirationCount(now, later)).toBe('less than a minute!');
   });
 });
--- src/frontend/bookmarkslimbo/src/app/__pipes/expiration-count.pipe.ts
+++ src/frontend/bookmarkslimbo/src/app/__pipes/expiration-count.pipe.ts
@@ -1,4 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import * as moment from 'moment';
+
+const units = ['year', 'month', 'day', 'hour', 'minute'];
 
 @Pipe({
   name: 'expirationCount'
@@ -6,7 +9,51 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ExpirationCountPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    return '1 days, 3 hours';
+    return this.expirationCount(moment(), moment(value));
   }
 
+  expirationCount(now: moment.Moment, later: moment.Moment) {
+    const diffs = units.map(unit => this.diffOrNull(now, later, unit))
+    const firstNotNull = diffs.indexOf(diffs.filter(v => null != v)[0]);
+    return firstNotNull != -1
+      ? [diffs[firstNotNull], diffs[firstNotNull + 1]]
+          .filter(v => null != v)
+          .reduce((a, b) => `${a}, ${b}`)
+      : 'less than a minute!';
+
+  }
+
+  private diffOrNull(now: moment.Moment, later: moment.Moment, unit: string): string | null {
+    const diff = this.diff(now, later, unit);
+    return diff > 0
+      ? `${diff} ${unit}s`
+      : null;
+  }
+  
+  private diff(now: moment.Moment, later: moment.Moment, unit): number {
+    if (unit === 'day') {
+      return this.daysDiff(now, later);
+    }
+    const diff = later.diff(now, unit);
+    const modulo = this.modulo(unit);
+    return modulo ? diff % modulo : diff;
+  }
+
+  private daysDiff(now: moment.Moment, later: moment.Moment): number {
+    if (later.date() >= now.date()) {
+      const laterMonth = later.get('month');
+      return later.diff(moment(now).month(laterMonth), 'day');
+    } else {
+      return later.date() + (now.daysInMonth() - now.date());
+    }
+  }
+
+  private modulo(unit): number | null {
+    return {
+      'year': null,
+      'month': 12,
+      'hour': 24,
+      'minute': 60,
+    }[unit];
+  }
 }
