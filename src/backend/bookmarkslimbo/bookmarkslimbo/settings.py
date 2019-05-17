8e7be7146396446359455abd59168e1c6e4edf3c
kamil-s-solecki <kamsolecki@gmail.com>
2019-05-17 10:58:22 +0200

    Configure db from url

--- src/backend/bookmarkslimbo/bookmarkslimbo/settings.py
+++ src/backend/bookmarkslimbo/bookmarkslimbo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -87,14 +88,7 @@ if DEBUG:
     }
 else:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ['DATABASE_NAME'],
-            'USER': os.environ['DATABASE_USER'],
-            'PASSWORD': os.environ['DATABASE_PASSWORD'],
-            'HOST': os.environ['DATABASE_HOST'],
-            'PORT': '5432',
-        }
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
 
 # Password validation
